e32b186c3a542bba329875585e4a9d9dd5c6336c
Emilymatoke <emilymomata@gmail.com>
2024-03-13 14:46:02 +0300

    Added deliverables

---
+++ magazine.py
@@ -0,0 +1,32 @@
+class Magazine:
+    def __init__(self, name, category):
+        self._name = None
+        self._category = None
+        self.name = name
+        self.category = category
+
+    @property
+    def name(self):
+        """The name property"""
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        """Name must be a string between 2 and 16 characters in length"""
+        if isinstance(name, str) and 2 < len(name) > 16 :
+            self._name = name
+        else :
+            raise ValueError("Name must be between 2 and 16 characters")
+        
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, category):
+        if isinstance(category, str) and len(category) > 0:
+            self._category = category
+        else :
+            raise ValueError("Category must have at least one character")
+        
