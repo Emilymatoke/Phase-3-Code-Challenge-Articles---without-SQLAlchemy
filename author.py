50209788f2826848c0d39fbe264c6a6751b92c5d
Emilymatoke <emilymomata@gmail.com>
2024-03-13 15:35:14 +0300

    Added deliverables

--- author.py
+++ author.py
@@ -1,6 +1,10 @@
 class Author :
-    def __init__(self, name) :
+    def __init__(self, name) :   
         self.name = name
+        self._name = None
+        self._articles = []
+
+        
 
     @property
     def name(self) :
@@ -19,4 +23,21 @@ class Author :
         if hasattr(self, name):
             raise AttributeError("Attribute '{}' cannot be modified".format(name))
         super().__setattr__(name, value)
-        
+
+
+    def articles(self):
+        return self._articles
+
+    def magazines(self):
+        return list(set(article.magazine for article in self._articles))
+
+    def add_article(self, magazine, title):
+        article = Article(self, magazine, title)
+        self._articles.append(article)
+        return article
+
+    def topic_areas(self):
+        categories = set()
+        for article in self._articles:
+            categories.add(article.magazine.category)
+        return list(categories) if categories else None
